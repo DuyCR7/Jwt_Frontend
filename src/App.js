a03e9a3056a63f6ec0fac827f57e1ccddaab2de4
DuyCR7 <anhduy0317@gmail.com>
2024-06-20 09:30:46 +0700

    cài đặt react-router-dom bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss";
+import Login from "./components/Login/Login";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Nav />
+        <Switch>
+          <Route path="/news">news</Route>
+          <Route path="/contact">contact</Route>
+          <Route path="/about">about</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            home
+          </Route>
+          <Route path="*">404 Not Found</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = (props) => {
+    return (
+        <div className='login-container'>
+            Login
+        </div>
+    );
+}
+
+export default Login;
