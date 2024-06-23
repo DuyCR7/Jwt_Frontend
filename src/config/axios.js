50f380e285689164333fb67215e20476ce8140ed
DuyCR7 <anhduy0317@gmail.com>
2024-06-23 17:35:33 +0700

    set global axios instance.defaults.withCredentials = true;

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,6 +5,9 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080'
 });
 
+// các request gửi lên server sẽ đính kèm cookie vào
+instance.defaults.withCredentials = true;
+
 // Alter defaults after instance has been created
 // instance.defaults.headers.common['Authorization'] = "AUTH_TOKEN";
 
