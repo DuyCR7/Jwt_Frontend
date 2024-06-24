eb1e0270104fecd5f95ebdd9ae2518655275ca7c
DuyCR7 <anhduy0317@gmail.com>
2024-06-24 16:06:10 +0700

    Design Roles

---
+++ src/components/Role/Role.js
@@ -0,0 +1,85 @@
+import "./Role.scss";
+import {useState} from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+const Role = (props) => {
+
+    const [listChild, setListChild] = useState({
+        child_1: {
+            url: '',
+            description: ''
+        }
+    });
+
+    const handleOnchangeInput = (name, value, key) => {
+        let _listChild = _.cloneDeep(listChild);
+
+        _listChild[key][name] = value;
+        setListChild(_listChild);
+    }
+
+    const handleAddNewInput = () => {
+        let _listChild = _.cloneDeep(listChild);
+        _listChild[`child_${uuidv4()}`] = {
+            url: '',
+            description: ''
+        };
+        setListChild(_listChild);
+    }
+
+    const handleDeleteInput = (key) => {
+        let _listChild = _.cloneDeep(listChild);
+        delete _listChild[key];
+        setListChild(_listChild);
+    }
+
+    return (
+        <>
+            <div className="role-container">
+                <div className="container">
+                    <div className="mt-3">
+                        <div className="title">
+                            <h4>Add a new role</h4>
+                        </div>
+                        <div className="role-parent">
+                            {
+                                Object.entries(listChild).map(([key, child], index) => {
+                                    return (
+                                            <div className="row role-child" key={`child-${key}`}>
+                                                <div className={`col-5 form-group ${key}`}>
+                                                    <label>URL:</label>
+                                                    <input type="text" className="form-control" value={child.url}
+                                                            onChange={(e) => handleOnchangeInput("url", e.target.value, key)}/>
+                                                </div>
+                                                <div className="col-5 form-group">
+                                                    <label>Description:</label>
+                                                    <input type="text" className="form-control" value={child.description}
+                                                            onChange={(e) => handleOnchangeInput("description", e.target.value, key)}/>
+                                                </div>
+                                                <div className="col-2 mt-4">
+                                                    <button className="btn btn-primary me-3" onClick={() => handleAddNewInput()}>
+                                                        <i className="fa fa-plus-circle"></i>
+                                                    </button>
+                                                    {index >= 1 &&
+                                                    <button className="btn btn-warning" onClick={() => handleDeleteInput(key)}>
+                                                        <i className="fa fa-trash"></i>
+                                                    </button>
+                                                    }
+                                                </div>
+                                            </div>
+                                    )
+                                })
+                            }
+                            <div>
+                                <button className="btn btn-success mt-3">Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Role;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,12 +3,14 @@ import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
 import Users from "../components/ManageUsers/Users";
 import PrivateRoutes from "./PrivateRoutes";
+import Role from "../components/Role/Role";
 
 const AppRoutes = (props) => {
     return (
         <>
             <Switch>
                 <PrivateRoutes path="/users" component={<Users />} />
+                <PrivateRoutes path="/roles" component={<Role />} />
                 <PrivateRoutes path="/projects" component={<Users />} />
 
                 <Route path="/login">
